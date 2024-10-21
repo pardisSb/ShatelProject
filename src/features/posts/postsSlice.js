24c160697094c901a6a04840dccb376986ea2245
ParDs <user@Pardiss-MacBook-Pro.local>
2024-10-21 11:00:06 +0330

    feat: add posts list and posts slice

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchData = createAsyncThunk("data/posts", async () => {
+  const response = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  return response.data;
+});
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState: {
+    posts: [],
+    status: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchData.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchData.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.posts = action.payload;
+      })
+      .addCase(fetchData.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default postsSlice.reducer;
